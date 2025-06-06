e56662ec3cf07efaaa2da61ffba7ef133bc956cf
mehdi720 <81516214+mehdi720@users.noreply.github.com>
2025-06-06 17:54:42 +0800

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -382,7 +382,7 @@ const Welcome: FC<IWelcomeProps> = ({
               </div>}
             <a className='flex items-center pr-3 space-x-3' href="https://gpt.yuychat.cn" target="_blank">
               <span className='uppercase'>{t('app.chat.powerBy')}</span>
-              <FootLogo />
+              <img src="/yangyang.png" alt="Logo" className="h-5" />
             </a>
           </div>
         )}
